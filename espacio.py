dbaba01418b8a469278fc6d3b5e6c5df0d84fbcb
Thiago3108 <santiago2234508@correo.uis.edu.co>
2023-05-19 15:25:49 -0400

    Se agrego el codigo de la nave

---
+++ espacio.py
@@ -0,0 +1,103 @@
+#---------------------------------
+# Desktop app -Estudiante 
+#---------------------------------
+
+# se importa la libreria tkinter con todas sus funciones
+from tkinter import *   
+import random
+import tkinter as tk
+
+#variables globales
+BASE=660
+ALTURA=460
+
+#def variables
+
+def mover_objeto(x, y):
+    reiniciar_circulos()
+    c.move(objeto, x, y)
+
+def reiniciar_circulos():
+    c.delete("circulo") 
+    for i in range(600):
+        x_estrella = random.randint(0, BASE - 20)
+        y_estrella = random.randint(0, ALTURA - 20)
+        color = "#"
+        for caracter in range(6):   
+            color = color + random.choice("0123456789ABCDF")
+            tamaño = random.randint(0,10)
+        c.create_oval(x_estrella,y_estrella, x_estrella+tamaño, y_estrella+tamaño, fill = color, tags="circulo")
+def mover_arriba(event=None):
+    mover_objeto(0, -10)
+    
+def mover_abajo(event=None):
+    mover_objeto(0, 10) 
+
+def mover_izquierda(event=None):
+    mover_objeto(-10, 0)  
+
+def mover_derecha(event=None):
+    mover_objeto(10, 0) 
+
+#-----------------------------
+# ventana principal de la app
+#-----------------------------
+# se declara una variable llamada ventana_principal, que adquiere las caracteristicas de un objeto Tk()
+ventana_principal = Tk()
+# titulo de la ventana
+ventana_principal.title("Graficas 2D - Lineas rectas")
+# tamaño de la ventana
+ventana_principal.geometry("700x650")
+# deshabilitar boton de maximizar
+ventana_principal.resizable(False, False)
+# color de fondo de la ventana
+ventana_principal.config(bg="white")
+
+#--------------------------------
+# frame graficacion
+#--------------------------------
+frame_graficacion = Frame(ventana_principal)
+frame_graficacion.config(bg="black", width=680, height=480)
+frame_graficacion.place(x=10, y=10)
+
+#canvas
+
+c = Canvas(frame_graficacion, width=BASE, height=ALTURA)
+c.configure(bg="black")
+c.place(x=10, y=10)
+
+#objeto o balon 
+image = PhotoImage(file="nave (1).png")
+objeto = c.create_image(100,200, image=image)
+
+#frame de controles
+frame_controles = Frame(ventana_principal)
+frame_controles.configure(bg="khaki", width=680, height=140)
+frame_controles.place(x=10, y = 500)
+
+#barra de arriva
+boton_mover_arriva= Button(frame_controles, command=mover_arriba)
+boton_mover_arriva.configure(bg = "orange",text="⬆", width=2, height=0)
+boton_mover_arriva.place(x=290,y=30)
+#barra de la derecha
+boton_mover_derecha= Button(frame_controles, command=mover_derecha)
+boton_mover_derecha.configure(bg = "orange",text="➡", width=2, height=0)
+boton_mover_derecha.place(x=335,y=45)
+#barra de la izquierda
+boton_mover_izquierda= Button(frame_controles, command=mover_izquierda)
+boton_mover_izquierda.configure(bg = "orange",text="⬅", width=2, height=0)
+boton_mover_izquierda.place(x=245,y=45)
+#barra pa abajo
+boton_mover_abajo= Button(frame_controles, command=mover_abajo)
+boton_mover_abajo.configure(bg = "orange",text="⬇", width=2, height=0)
+boton_mover_abajo.place(x=290,y=60)
+   
+#teclas
+ventana_principal.bind("<Up>", mover_arriba)
+ventana_principal.bind("<Down>", mover_abajo)
+ventana_principal.bind("<Left>", mover_izquierda)
+ventana_principal.bind("<Right>", mover_derecha)
+
+
+
+ventana_principal.mainloop()
--- pruebas.py
+++ pruebas.py
@@ -1,93 +1,54 @@
-"""
-from tkinter import *
-
-ventana = Tk()
-
-canvas = Canvas(ventana, width=200, height=200)
-canvas.pack()
-
-# Crear un rectángulo sin color de fondo
-rectangulo = canvas.create_rectangle(50, 50, 150, 150, outline="black")
-
-ventana.mainloop()
-########################################################################
 import tkinter as tk
 import random
 
-def mover_objeto():
-    canvas.move(objeto, random.randint(-5, 5), random.randint(-5, 5))
-    # Mueve el objeto en coordenadas aleatorias entre -5 y 5 en x e y
-    ventana.after(1000, mover_objeto)  # Mueve el objeto cada segundo
+def mover_objeto(dx, dy):
+    c.move(objeto, dx, dy)
+    x1, y1, x2, y2 = c.bbox(objeto)  # Obtener las coordenadas del objeto
+    if x1 < 0 or y1 < 0 or x2 > ancho or y2 > alto:  # Verificar si el objeto está fuera del rango de visión
+        reiniciar_objeto()
+
+def reiniciar_objeto():
+    c.delete(objeto)  # Eliminar el objeto actual
+    x = random.randint(50, ancho - 50)
+    y = random.randint(50, alto - 50)
+    nuevo_objeto = c.create_rectangle(x, y, x+50, y+50, fill="blue")  # Crear un nuevo objeto
+    return nuevo_objeto
+
+def mover_arriba(event=None):
+    mover_objeto(0, -10)
+
+def mover_abajo(event=None):
+    mover_objeto(0, 10)
+
+def mover_izquierda(event=None):
+    mover_objeto(-10, 0)
+
+def mover_derecha(event=None):
+    mover_objeto(10, 0)
 
 ventana = tk.Tk()
-canvas = tk.Canvas(ventana, width=400, height=400)
-canvas.pack()
+ancho = 400
+alto = 400
+c = tk.Canvas(ventana, width=ancho, height=alto)
+c.pack()
 
-objeto = canvas.create_rectangle(50, 50, 100, 100, fill="red")
+objeto = c.create_rectangle(50, 50, 100, 100, fill="blue")
 
-mover_objeto()
+boton_arriba = tk.Button(ventana, text="Arriba", command=mover_arriba)
+boton_arriba.pack()
+
+boton_abajo = tk.Button(ventana, text="Abajo", command=mover_abajo)
+boton_abajo.pack()
+
+boton_izquierda = tk.Button(ventana, text="Izquierda", command=mover_izquierda)
+boton_izquierda.pack()
+
+boton_derecha = tk.Button(ventana, text="Derecha", command=mover_derecha)
+boton_derecha.pack()
+
+ventana.bind("<Up>", mover_arriba)
+ventana.bind("<Down>", mover_abajo)
+ventana.bind("<Left>", mover_izquierda)
+ventana.bind("<Right>", mover_derecha)
 
 ventana.mainloop()
-"""
-
-from tkinter import *
-import random
-import math
-
-# Definición de variables
-BASE = 660
-ALTURA = 460
-MOVIMIENTO_STEP = 2
-FPS = 60
-
-def mover_objeto():
-    x, y = c.coords(objeto)
-    objetivo_x = random.randint(30, BASE - 30)
-    objetivo_y = random.randint(30, ALTURA - 30)
-    
-
-    distancia = math.sqrt((objetivo_x - x) ** 2 + (objetivo_y - y) ** 2)
-    if distancia > MOVIMIENTO_STEP:
-        dx = int((objetivo_x - x) * MOVIMIENTO_STEP / distancia)
-        dy = int((objetivo_y - y) * MOVIMIENTO_STEP / distancia)
-    else:
-        dx = objetivo_x - x
-        dy = objetivo_y - y
-    
-    c.move(objeto, dx, dy)
-    ventana_principal.after(int(1000 / FPS), mover_objeto)
-
-# Ventana principal de la app
-ventana_principal = Tk()
-ventana_principal.title("Graficas 2D - Lineas rectas")
-ventana_principal.geometry("700x600")
-ventana_principal.resizable(False, False)
-ventana_principal.config(bg="white")
-
-# Frame de graficación
-frame_graficacion = Frame(ventana_principal, bg="green", width=680, height=480)
-frame_graficacion.place(x=10, y=10)
-
-# Canvas
-c = Canvas(frame_graficacion, width=BASE, height=ALTURA, bg="green")
-c.place(x=10, y=10)
-
-# Decoración de la cancha
-lineacentro = c.create_line(BASE/2, 0, BASE/2, ALTURA, fill="white", width=5)
-rectangulo = c.create_rectangle(0, 0, BASE, ALTURA, outline="white", width=5)
-elipse = c.create_oval((BASE/2)-50, (ALTURA/2)-50, (BASE/2)+50, (ALTURA/2)+50, outline="white", width=5)
-
-# Objeto o balón
-image = PhotoImage(file="balon.png")
-objeto = c.create_image(random.randint(30, BASE-30), random.randint(30, ALTURA-30), image=image)
-
-# Frame de controles
-frame_controles = Frame(ventana_principal, bg="khaki", width=680, height=90)
-frame_controles.place(x=10, y=500)
-
-# Botón de mover
-boton_mover = Button(frame_controles, command=mover_objeto, bg="orange", text="Mover", width=10, height=0)
-boton_mover.place(x=290, y=30)
-
-ventana_principal.after(int(1000 / FPS), mover_objeto)
-ventana_principal.mainloop()
